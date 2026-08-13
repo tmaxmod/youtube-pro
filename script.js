2f417ae1d98d524126020fb281161bc6563f2af6
tmaxmod <165796265+tmaxmod@users.noreply.github.com>
2026-08-13 19:51:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -717,16 +717,6 @@ color:${c};
 ytpSetI.innerHTML+=`<br><b style='font-size:18px' >YT PRO Settings</b>
 <span style="font-size:10px">v${YTProVer}</span>
 <br><br>
-<div data-action="follow" style="min-height:35px;height:auto;width:95%;margin:auto;background:#ee2a7b44;border-radius:30px;margin-bottom:15px;border:1px solid #ee2a7b;display:flex;padding:5px;gap:8px;">
-
-<img style="flex-shrink: 0;height:40px;width:40px;border-radius:50%;" src="https://raw.githubusercontent.com/prateek-chaubey/YTPro/refs/heads/main/.github/img/habitius.webp" >
-<div style="display:flex;flex-direction:column;align-items:flex-start;height:100%;width:auto;flex-shrink:0;font-size:14px;background:re;padding:0;"><b>Please follow Habitius on Instagram</b>For daily habit,lifestyle and health tips </div>
-
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="${isD ? "#ccc" : "#444"}" viewBox="0 0 16 16">
-<path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
-</svg>
-
-</div>
 
 <div><input type="url" placeholder="Enter Youtube URL" id="ytproUrlInput" ></div>
 <br>
@@ -815,16 +805,7 @@ ${localStorage.getItem("prompt")}
 
 
 <div class="credit" >
-<z style="margin-right:6px">Made with </z>
-
-<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#fff" viewBox="-1 -1 18 18">
-<path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" 
-stroke="black" ${ !isD ? "stroke-width='1'" : "" } stroke-linejoin="round" stroke-linecap="round"/>
-</svg>
-
-
-
-<z style="margin-left:6px">by TMAX MOD [ 🎧⚡ TUFAN 💤🔥 ]</z>
+<z style="margin-left:6px">TMAX MOD [ 🎧⚡ TUFAN 💤🔥 ]</z>
 </div>
 `;
 
